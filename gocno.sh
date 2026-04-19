6ffc9815cf3bd3855891921f30f040b62bcdfa75
ImEX21 <nikita.pyroschkin@gmail.com>
2026-04-19 19:18:05 +0300

    fix awww

--- gocno.sh
+++ gocno.sh
@@ -3,4 +3,4 @@ set -e
 sudo pacman -Syu
 sudo pacman -S  hyprland uwsm waybar swww fastfetch ttf-jetbrains-mono-nerd pavucontrol kitty rofi
 cp -r hypr kitty fastfetch rofi ~/.config/
-swww img k2af87zzsvze1.jpeg
+awww img k2af87zzsvze1.jpeg
