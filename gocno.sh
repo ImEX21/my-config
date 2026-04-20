62ccc08bf42984353057e568104bb6476fd01fcb
фанинажопа <faninpop18@gmail.com>
2026-04-20 12:25:29 +0000

    Update gocno.sh

--- gocno.sh
+++ gocno.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 sudo pacman -Syu
-sudo pacman -S  hyprland uwsm waybar swww fastfetch ttf-jetbrains-mono-nerd pavucontrol kitty rofi
+sudo pacman -S  hyprland uwsm waybar swww fastfetch ttf-jetbrains-mono-nerd pavucontrol kitty rofi pipewire pipewire-pulse pipewire-jack pipewire-audio wireplumber pipewire-alsa
 rm -rf ~/.config/hypr
 rm -rf ~/.config/fastfetch
 rm -rf ~/.config/rofi
@@ -9,3 +9,4 @@ rm -rf ~/.config/kitty
 
 cp -r hypr kitty fastfetch rofi ~/.config/
 awww img k2af87zzsvze1.jpeg
+echo "установка завершена,рекомендуеться рестарт системы."
