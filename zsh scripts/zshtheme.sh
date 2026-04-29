6af860bdf4041f5077796b61e7830fef82138139
фанинажопа <faninpop18@gmail.com>
2026-04-29 06:46:07 +0500

    Update zshtheme.sh

--- zsh scripts/zshtheme.sh
+++ zsh scripts/zshtheme.sh
@@ -7,5 +7,5 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 cd "$(dirname "$0")"
 rm -rf ~/.zshrc
-cp -r .zshrc ~
+mv .zshrc ~
 echo "good! Restart please. "
