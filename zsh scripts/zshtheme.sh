33924051a9cef3cc4fd3d3f2af68a1f35aaf8cad
фанинажопа <faninpop18@gmail.com>
2026-04-30 13:00:50 +0500

    Update zshtheme.sh

--- zsh scripts/zshtheme.sh
+++ zsh scripts/zshtheme.sh
@@ -8,4 +8,6 @@ echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 cd "$(dirname "$0")"
 rm -rf ~/.zshrc
 mv .zshrc ~
+echo "done! To configure zsh: p10k configure."
+sleep 2
 echo "good! Restart please. "
