190bfc1717cd80c0df94e1f6bc27e13ec85f4d6d
фанинажопа <faninpop18@gmail.com>
2026-04-30 12:59:08 +0500

    Update gocno.sh

--- gocno.sh
+++ gocno.sh
@@ -3,14 +3,16 @@ set -e
 cd "$(dirname "$0")"
 sudo pacman -Syu
 sudo pacman -S --noconfirm hyprland uwsm waybar awww fastfetch \
-ttf-jetbrains-mono-nerd pavucontrol kitty rofi \
-pipewire pipewire-pulse pipewire-jack \
+ttf-jetbrains-mono-nerd pavucontrol kitty rofi noto-fonts noto-fonts-emoji \
+pipewire pipewire-pulse pipewire-jack pipewire-alsa \
 wireplumber pipewire-alsa hyprshot nerd-fonts swaync dolphin
 rm -rf ~/.config/hypr
 rm -rf ~/.config/fastfetch
 rm -rf ~/.config/rofi
 rm -rf ~/.config/kitty
 cp -r hypr kitty fastfetch rofi ~/.config/
+echo "поставить обои - awww img (путь до обоев)"
+sleep 2
 echo "Установка завершена, рестарт системы."
 sleep 2
 sudo reboot
